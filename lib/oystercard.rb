b5a3f7698c7686ed5fbfa92e8821daf3013b8fb3
SamJamCul <samuel.culley@digital.cabinet-office.gov.uk>
2018-11-06 15:44:34 +0000

    adds error message for overtopping

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,21 @@
 class Oystercard
   attr_reader :balance
+  MAXIMUM_BALANCE = 90
 
   def initialize
     self.balance = 0
   end
 
   def top_up(amount)
+    fail "Max balance of #{MAXIMUM_BALANCE} exceeded" if go_over_max?(amount)
     self.balance += amount
   end
-
   private
 
   attr_writer :balance
+
+  def go_over_max?(amount)
+    amount + balance > MAXIMUM_BALANCE
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,5 +9,12 @@ describe Oystercard do
     it 'can top up the balance' do
       expect { subject.top_up 1 }.to change { subject.balance }.by 1
     end
+
+    it 'raises an error if the maximum balance is exceeded' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      error = "Max balance of #{maximum_balance} exceeded"
+      expect { subject.top_up(1) }.to raise_error error
+    end
   end
 end
