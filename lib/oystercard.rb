344e9f1d95e61f8a30ca362a99b3c07d8ed9733c
Al <>
2018-11-06 15:26:09 +0000

    added method to top up oystercard balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,16 @@
 class Oystercard
   attr_reader :balance
- 
+
   def initialize
     self.balance = 0
   end
 
+  def top_up(amount)
+    self.balance += amount
+  end
+
   private
- 
+
   attr_writer :balance
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,10 @@ describe Oystercard do
   it 'has a balance of zero' do
     expect(subject.balance).to eq (0)
   end
+
+  describe '#top_up' do
+    it 'can top up the balance' do
+      expect { subject.top_up 1 }.to change { subject.balance }.by 1
+    end
+  end
 end
